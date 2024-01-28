c802047e67ff0353af1a261906e0ea0c66fb34f1
Plaban <turjoyplaban09@gmail.com>
2024-01-28 18:06:59 +0600

    Document is Formatted

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
-var typed= new Typed(".text", {
-    strings:["Student","Web and App Developer","Personal Investors"],
-    typeSpeed:100,
-    backSpeed:100,
-    backDelay:1000,
-    loop:true
+var typed = new Typed(".text", {
+    strings: ["Student", "Web and App Developer", "Personal Investors"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
 });
